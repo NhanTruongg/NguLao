ab7f545bb33e817655c1cced5d2d3a53b2e166f3
NhanTruongg <truongnghianhan76@gmail.com>
2026-03-06 19:41:38 +0700

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,573 @@
+getgenv().Key = "ERTNgbSoGhGUchHnUWgFrkimaYrvMEhh"
+getgenv().ImportMacro = "https://raw.nousigi.com/macro/1188097087750021143_09948d0a11df3a4ff6b01c24a8dab5a2.json?macroname=winter140_3"
+getgenv().Config = {
+	["Auto Join Equipper"] = {
+		["Macro Equipper"] = {
+			["Enable"] = false
+		},
+		["Team Equipper"] = {
+			["Enable"] = false
+		}
+	},
+	["Gold Buyer"] = {
+		["Enable"] = false
+	},
+	["Dungeon Joiner"] = {
+		["Auto Join"] = false
+	},
+	["Stage Joiner"] = {
+		["Join Highest"] = false,
+		["Nightmare Mode"] = false,
+		["Auto Join"] = false,
+		["Join Lowest Clear"] = false
+	},
+	["Macros"] = {
+		["Macro Retry Limit"] = 0,
+		["Ignore Macro Timing"] = true,
+		["No Ignore Sell Timing"] = true,
+		["Auto Equip"] = false,
+		["Winter Event Macro"] = {
+			["Zombie Mode"] = "winter140_3"
+		},
+		["Macro"] = "winter140_3",
+		["Play"] = true
+	},
+	["Webhook"] = {
+		["Unit Summoned"] = false,
+		["Trait Rerolled"] = false,
+		["URL"] = "https://discord.com/api/webhooks/1126869149583425578/CPCwtmPK1nJGm5TH3SE8I2FkR1pmf8T1V28Mq8ju9Dv9zwWHAutXBLeqPwJgrPvbbaGz",
+		["Unit Stat Potential"] = false,
+		["Stage Finished"] = true,
+		["Stat Potential Rerolled"] = false,
+		["Match Restarted"] = true
+	},
+	["Legend Stage Joiner"] = {
+		["Auto Join"] = false
+	},
+	["Trait Reroller"] = {
+		["Enable"] = false
+	},
+	["Odyssey Joiner"] = {
+		["Second Team"] = 2,
+		["Auto Join"] = false,
+		["Cash Out Floor"] = 5,
+		["Intensity"] = 200,
+		["First Team"] = 1
+	},
+	["Claimer"] = {
+		["Auto Claim Battle Pass"] = false,
+		["Auto Claim Quest"] = false,
+		["Auto Claim Collection Milestone"] = false,
+		["Auto Claim Achievement"] = false,
+		["Auto Claim Daily Reward"] = false,
+		["Auto Claim Collection"] = false,
+		["Auto Claim Enemy Index"] = false,
+		["Auto Claim Milestone"] = false
+	},
+	["Gameplay"] = {
+		["Occult Hunt"] = {
+			["Collect Talisman"] = false,
+			["Use All Talisman"] = {
+				["Enable"] = false,
+				["Wave"] = 1
+			},
+			["Use Talisman on Crab"] = false
+		},
+		["Frozen Volcano"] = {
+			["Sell Placed Friezo Unit"] = false,
+			["Auto Friezo"] = false,
+			["Leave Extra Money"] = 5000,
+			["Upgrade Amount"] = 0,
+			["Auto Save Brolzi"] = false
+		},
+		["Double Dungeon"] = {
+			["Auto Statue"] = false,
+			["Leave Extra Money"] = 5000,
+			["Upgrade Amount"] = 0
+		},
+		["Auto Sell"] = {
+			["Auto Sell Farm"] = {
+				["Enable"] = false,
+				["Wave"] = 1,
+				["Stage Type"] = {
+					["Odyssey"] = true,
+					["Challenge"] = true,
+					["Portal"] = true,
+					["Worldline"] = true,
+					["Legend Stage"] = true,
+					["BossEvent"] = true,
+					["Dungeon"] = true,
+					["Infinite"] = true,
+					["Rift"] = true,
+					["Story"] = true,
+					["Raid"] = true
+				}
+			},
+			["Auto Sell Unit"] = {
+				["Enable"] = false,
+				["Wave"] = 1,
+				["Stage Type"] = {
+					["Odyssey"] = true,
+					["Challenge"] = true,
+					["Portal"] = true,
+					["Worldline"] = true,
+					["Legend Stage"] = true,
+					["BossEvent"] = true,
+					["Dungeon"] = true,
+					["Infinite"] = true,
+					["Rift"] = true,
+					["Story"] = true,
+					["Raid"] = true
+				}
+			}
+		},
+		["Ant Island"] = {
+			["Auto Plug Ant Tunnel"] = false
+		},
+		["Shibuya Station"] = {
+			["Upgrade Amount"] = 0,
+			["Leave Extra Money"] = 5000,
+			["Auto Mohato"] = false
+		},
+		["Ruined City"] = {
+			["Use Mount to Travel"] = true,
+			["Active Tower"] = false,
+			["Unhandcuff"] = false
+		},
+		["Auto Vote Start"] = true,
+		["Saber Event"] = {
+			["Auto Select Servant"] = false,
+			["Servant"] = "Berserker"
+		},
+		["Steel Ball Run"] = {
+			["Collect Steel Ball"] = false
+		},
+		["Random Sacrifice Domain"] = {
+			["Sell Units on Event"] = false
+		},
+		["Edge of Heaven"] = {
+			["Auto Join Lfelt Portal"] = false,
+			["Pause instead of Joining"] = false
+		},
+		["Auto Ability"] = {
+			["Song Jinwu and Igros"] = {
+				["Auto Shadow"] = {
+					["Enable"] = false,
+					["Order"] = {
+						["Steel"] = 2,
+						["Bear"] = 1,
+						["Healer"] = 3,
+						["Belu"] = 4
+					}
+				}
+			},
+			["Hellkiller (Slayer)"] = {
+				["Disable First Person"] = false,
+				["Auto Shoot"] = false
+			},
+			["Tempest Pirate (Navigator)"] = {
+				["Auto Dig Chest"] = false
+			},
+			["Iscanur (Pride)"] = {
+				["Burn Units"] = {
+					["Enable"] = false,
+					["Slots"] = {
+						["1"] = false,
+						["3"] = false,
+						["2"] = false,
+						["5"] = false,
+						["4"] = false,
+						["6"] = false
+					},
+					["Stage Type"] = {
+						["Odyssey"] = true,
+						["Challenge"] = true,
+						["Portal"] = true,
+						["Worldline"] = true,
+						["Legend Stage"] = true,
+						["BossEvent"] = true,
+						["Dungeon"] = true,
+						["Infinite"] = true,
+						["Rift"] = true,
+						["Story"] = true,
+						["Raid"] = true
+					}
+				},
+				["Ultimate"] = {
+					["Enable"] = false
+				}
+			},
+			["Koguro (Unsealed)"] = {
+				["Elemental Dimensions"] = {
+					["Enable"] = false,
+					["Order"] = {
+						["Fire"] = 1,
+						["Ice"] = 2,
+						["Sand"] = 3
+					}
+				}
+			},
+			["Auto Use Ability"] = false,
+			["God (Standless)"] = {
+				["Reality Rewrite"] = {
+					["Enable"] = false,
+					["Status Effect"] = "Slow"
+				}
+			},
+			["Skele King (Rock)"] = {
+				["Auto Guitar"] = {
+					["Auto King of String"] = false,
+					["Perfect Chance"] = 90,
+					["Enable"] = false
+				}
+			}
+		},
+		["Auto Skip Wave"] = {
+			["Enable"] = true,
+			["Stop Skip Stage Type"] = {
+				["Odyssey"] = true,
+				["Challenge"] = true,
+				["Portal"] = true,
+				["Worldline"] = true,
+				["Legend Stage"] = true,
+				["BossEvent"] = true,
+				["Dungeon"] = true,
+				["Infinite"] = true,
+				["Rift"] = true,
+				["Story"] = true,
+				["Raid"] = true
+			},
+			["Stop at Wave"] = 0
+		},
+		["Auto Restart"] = {
+			["Enable"] = false,
+			["Wave"] = 140,
+			["Stage Type"] = {
+				["Odyssey"] = true,
+				["Challenge"] = true,
+				["Portal"] = true,
+				["Worldline"] = true,
+				["Legend Stage"] = true,
+				["BossEvent"] = true,
+				["Dungeon"] = true,
+				["Infinite"] = true,
+				["Rift"] = true,
+				["Story"] = true,
+				["Raid"] = true
+			}
+		},
+		["Martial Island"] = {
+			["Auto Join God Portal"] = false,
+			["Collect Rotara Earring"] = false,
+			["Pause instead of Joining"] = false,
+			["Restart if no Rotara Earring"] = false
+		}
+	},
+	["Daily Challenge Joiner"] = {
+		["Auto Join"] = false
+	},
+	["Misc"] = {
+		["Auto Open Gift Boxes"] = false,
+		["Right Click Move"] = false,
+		["Max Camera Zoom"] = 40,
+		["Redeem Code"] = false,
+		["Right Click Teleport"] = false,
+		["Auto Delete Portal"] = {
+			["Enable"] = false,
+			["Fall Portal"] = 500,
+			["Summer Portal"] = 500,
+			["Spring Portal"] = 500
+		}
+	},
+	["Summoner"] = {
+		["Unselect if Summoned"] = false,
+		["Auto Summon Selection"] = false,
+		["Delete Rarity"] = {
+			["Legendary"] = false,
+			["Mythic"] = false,
+			["Exclusive"] = false,
+			["Epic"] = false,
+			["Rare"] = false
+		},
+		["Auto Summon Special"] = false,
+		["Auto Summon Winter"] = false,
+		["Teleport Lobby new Banner"] = false,
+		["Auto Summon Winter Memoria"] = false,
+		["Delete Shiny Rarity"] = {
+			["Legendary"] = false,
+			["Mythic"] = false,
+			["Exclusive"] = false,
+			["Epic"] = false,
+			["Rare"] = false
+		}
+	},
+	["Unit Deleter"] = {
+		["Enable"] = false,
+		["Rarity"] = {
+			["Epic"] = false,
+			["Legendary"] = false,
+			["Rare"] = false
+		}
+	},
+	["Worldline Joiner"] = {
+		["Auto Join"] = false
+	},
+	["Regular Challenge Joiner"] = {
+		["Auto Join"] = false,
+		["Teleport Lobby new Challenge"] = false
+	},
+	["Auto Play"] = {
+		["Auto Upgrade"] = {
+			["Upgrade Order"] = {
+				["1"] = 1,
+				["3"] = 3,
+				["2"] = 2,
+				["5"] = 5,
+				["4"] = 4,
+				["6"] = 6
+			},
+			["Place and Upgrade"] = false,
+			["Enable"] = false,
+			["Focus on Farm"] = false,
+			["Upgrade Limit"] = {
+				["1"] = 0,
+				["3"] = 0,
+				["2"] = 0,
+				["5"] = 0,
+				["4"] = 0,
+				["6"] = 0
+			},
+			["Upgrade Method"] = "Hotbar left to right (until Max)"
+		},
+		["Place Limit"] = {
+			["1"] = 0,
+			["3"] = 0,
+			["2"] = 0,
+			["5"] = 0,
+			["4"] = 0,
+			["6"] = 0
+		},
+		["Enable"] = false,
+		["Place Wave"] = {
+			["1"] = 0,
+			["3"] = 0,
+			["2"] = 0,
+			["5"] = 0,
+			["4"] = 0,
+			["6"] = 0
+		},
+		["Place Order"] = {
+			["1"] = 1,
+			["3"] = 3,
+			["2"] = 2,
+			["5"] = 5,
+			["4"] = 4,
+			["6"] = 6
+		}
+	},
+	["Match Finished"] = {
+		["Auto Return Lobby"] = false,
+		["Auto Next"] = false,
+		["Replay Amount"] = 0,
+		["Return Lobby Failsafe"] = false,
+		["Auto Replay"] = true
+	},
+	["Crafter"] = {
+		["Enable"] = false,
+		["Teleport Lobby full Essence"] = false,
+		["Essence Stone"] = {
+			["Pink Essence Stone"] = true,
+			["Blue Essence Stone"] = true,
+			["Red Essence Stone"] = true,
+			["Yellow Essence Stone"] = true,
+			["Purple Essence Stone"] = true
+		},
+		["Essence Stone Limit"] = {
+			["Pink Essence Stone"] = 50,
+			["Blue Essence Stone"] = 50,
+			["Red Essence Stone"] = 50,
+			["Yellow Essence Stone"] = 50,
+			["Purple Essence Stone"] = 50
+		}
+	},
+	["Skin Deleter"] = {
+		["Enable"] = false
+	},
+	["Boss Event Joiner"] = {
+		["Auto Join"] = false,
+		["Nightmare Mode"] = false
+	},
+	["Stat Reroller"] = {
+		["Stat Potential"] = 100,
+		["Teleport Lobby reach Stat Potential"] = false,
+		["Enable"] = false,
+		["Type"] = {
+			["Damage"] = false,
+			["All"] = false,
+			["Range"] = false,
+			["SPA"] = false
+		},
+		["Stat"] = {
+			["神"] = false,
+			["Z+"] = false,
+			["S"] = false,
+			["Z"] = false
+		}
+	},
+	["Auto Join Setting"] = {
+		["Joiner Priority"] = {
+			["Boss Event Joiner"] = 5,
+			["Legend Stage Joiner"] = 2,
+			["Raid Joiner"] = 3,
+			["Winter Event Joiner"] = 9,
+			["Stage Joiner"] = 1,
+			["Odyssey Joiner"] = 7,
+			["Weekly Challenge Joiner"] = 13,
+			["Worldline Joiner"] = 6,
+			["Limitless Odyssey Joiner"] = 8,
+			["Regular Challenge Joiner"] = 11,
+			["Boss Bounties Joiner"] = 10,
+			["Dungeon Joiner"] = 4,
+			["Daily Challenge Joiner"] = 12,
+			["Rift Joiner"] = 14
+		},
+		["Joiner Cooldown"] = 0
+	},
+	["Raid Joiner"] = {
+		["Auto Join"] = false
+	},
+	["Winter Event"] = {
+		["Winter Event Joiner"] = {
+			["Auto Join"] = true,
+			["Mode"] = "Regular"
+		}
+	},
+	["Failsafe"] = {
+		["Teleport Lobby if Player"] = false,
+		["Disable Auto Teleport AFK Chamber"] = true
+	},
+	["Boss Bounties Joiner"] = {
+		["Auto Join"] = false
+	},
+	["Weekly Challenge Joiner"] = {
+		["Auto Join"] = false
+	},
+	["Secure"] = {
+		["Random Offset"] = true,
+		["Walk Around"] = true
+	},
+	["Limitless Odyssey Joiner"] = {
+		["Auto Force Skip Wave"] = false,
+		["Auto Join"] = false,
+		["Force Skip Wave"] = 1,
+		["Intensity"] = 25,
+		["Leave Floor"] = 1
+	},
+	["Performance"] = {
+		["Delete Map"] = true,
+		["Boost FPS"] = true,
+		["Black Screen"] = true,
+		["Delete Entities"] = true
+	},
+	["Unit Feeder"] = {
+		["Auto Feed"] = false,
+		["Feed Level"] = 60
+	},
+	["Modifier"] = {
+		["Restart Modifier"] = {
+			["Enable"] = false,
+			["Stage Type"] = {
+				["Odyssey"] = true,
+				["Challenge"] = true,
+				["Portal"] = true,
+				["Worldline"] = true,
+				["Legend Stage"] = true,
+				["BossEvent"] = true,
+				["Dungeon"] = true,
+				["Infinite"] = true,
+				["Rift"] = true,
+				["Story"] = true,
+				["Raid"] = true
+			},
+			["Modifier"] = {
+				["Tyrant Destroyer"] = false,
+				["Money Surge"] = false,
+				["King's Burden"] = false,
+				["No Trait No Problem"] = false,
+				["Fisticuffs"] = false,
+				["Warding off Evil"] = false,
+				["Exterminator"] = false,
+				["Lifeline"] = false,
+				["Drowsy"] = false,
+				["Tyrant Arrives"] = false,
+				["High Class"] = false,
+				["Dodge"] = false,
+				["Quake"] = false,
+				["Immunity"] = false,
+				["Champions"] = false,
+				["Sphere Finder"] = false
+			}
+		},
+		["Auto Modifier"] = {
+			["Prioritize"] = {
+				["Tyrant Destroyer"] = 30,
+				["Range"] = 18,
+				["Revitalize"] = 6,
+				["Tyrant Arrives"] = 33,
+				["Exploding"] = 2,
+				["Immunity"] = 11,
+				["Restize"] = 34,
+				["Lifeline"] = 29,
+				["Regen"] = 7,
+				["Press It"] = 14,
+				["Shielded"] = 5,
+				["Cooldown"] = 19,
+				["Money Surge"] = 26,
+				["Strong"] = 3,
+				["Thrice"] = 4,
+				["Warding off Evil"] = 24,
+				["Quake"] = 9,
+				["Baroot"] = 35,
+				["Dodge"] = 10,
+				["Precise Attack"] = 13,
+				["Fisticuffs"] = 25,
+				["Slayer"] = 16,
+				["Taxation"] = 37,
+				["Drowsy"] = 8,
+				["Damage"] = 20,
+				["High Class"] = 32,
+				["No Trait No Problem"] = 23,
+				["Harvest"] = 17,
+				["Speech"] = 39,
+				["King's Burden"] = 27,
+				["Mobilize"] = 38,
+				["Common Loot"] = 21,
+				["Stave Theocracy"] = 36,
+				["Exterminator"] = 28,
+				["Champions"] = 12,
+				["Fast"] = 1,
+				["Planning Ahead"] = 15,
+				["Uncommon Loot"] = 22,
+				["Sphere Finder"] = 31
+			},
+			["Enable"] = false
+		}
+	},
+	["Performance Failsafe"] = {
+		["Teleport Lobby FPS below"] = {
+			["Enable"] = false,
+			["FPS"] = 5
+		}
+	},
+	["Rift Joiner"] = {
+		["Join Solo Only"] = false,
+		["Hop Server if no Rift Portal"] = false,
+		["Auto Join"] = false,
+		["Teleport Lobby Rift spawn"] = {
+			["Enable"] = false,
+			["Force teleport"] = false,
+			["Extra Time"] = 60
+		}
+	}
+}
+loadstring(game:HttpGet("https://nousigi.com/loader.lua"))() 
